0f2e3a57926eeef53e323f7cc2d02439d91ed66e
Vairamuthu <mvairamuthu2003@gmail.com>
2025-03-12 22:11:55 +0530

    Remove unused Resource model import from index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const Resource = require("./models/Resource");
 
 // Config
 dotenv.config();
